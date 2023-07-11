f38debedfb0a98e80052eb6c9dc26c34936dee63
afrid_hossain <95118044+afridkhan77@users.noreply.github.com>
2023-07-11 19:00:45 +0600

    Add files via upload

---
+++ operators/Data Type Practice.js
@@ -0,0 +1,20 @@
+
+var name = "John";
+
+var age = 25;
+
+var isStudent = true;
+
+var hobbies = ["reading", "painting", "hiking"];
+
+var address = {
+  street: "123 Main Street",
+  city: "Cityville",
+  country: "Countryland"
+};
+
+console.log("My name is " + John + ".");
+console.log("I am " + 25 + " years old.");
+console.log("I am a student: " + isStudent + ".");
+console.log("My hobbies are " + hobbies.join("reading,painting ") + "iking");
+console.log("I live at " + address.street + ",123 Main Street " + address.city + ", Cityville" + address.country + "ountryland");
---
+++ operators/operators.js
@@ -0,0 +1,11 @@
+
+var age = 25;
+
+var name = "Afrid hossain";
+var isStudent = true;
+
+var birthYear;
+
+console.log("My name is " + afrid + " and I am " + 25 + " years old.");
+console.log("I am a student: " + isStudent + "yes");
+console.log("I was born in " + birthYear + "1997");
